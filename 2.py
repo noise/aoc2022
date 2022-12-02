e568112595b1d800df793a5d7cf8a34049220b09
Bret Barker <bret@abitrandom.net>
2022-12-02 10:47:31 -0500

    day 2, p2

--- 2.py
+++ 2.py
@@ -18,17 +18,38 @@ round_scores = {'A X': 3, # rock, rock: draw
                 'C Y': 0, # scissors, paper: lose
                 'C Z': 3} # scissors, scissors: draw
 
+outcome_moves = {'A X': 'Z', # rock, lose: scissors
+                 'A Y': 'X', # rock, draw: rock
+                 'A Z': 'Y', # rock, win: paper
+         
+                 'B X': 'X', # paper, lose: rock
+                 'B Y': 'Y', # paper, draw: paper
+                 'B Z': 'Z', # paper, win: scissors
+         
+                 'C X': 'Y', # scissors, lose: paper
+                 'C Y': 'Z', # scissors, draw: scissors
+                 'C Z': 'X'} # scissors, win: rock
+
 
 def score(moves):
     s = 0
     for m in moves:
-        print(m)
+        #print(m)
         if m == '':
             continue
         s += move_scores[m[2]]
         s += round_scores[m]
     return s
 
+def score2(outcomes):
+    s = 0
+    for o in outcomes:
+        if o == '':
+            continue
+        move = outcome_moves[o]
+        s += move_scores[move]
+        s += round_scores[o[0] + ' ' + move]
+    return s
 
 # sample
 print(score('''A Y
@@ -38,3 +59,13 @@ C Z'''.split('\n')))
 # real input
 inp = open("2.txt").read()
 print(score(inp.split('\n')))
+
+# sample
+print(score2('''A Y
+B X
+C Z'''.split('\n')))
+
+# real input
+inp = open("2.txt").read()
+print(score2(inp.split('\n')))
+
