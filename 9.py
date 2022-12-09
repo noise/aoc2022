fb8a9cf1fd2ae8d835027aeb44645f212e634ca6
Bret Barker <bret@abitrandom.net>
2022-12-09 09:23:14 -0500

    tidy 9

--- 9.py
+++ 9.py
@@ -14,35 +14,19 @@ def parse(inp):
     return [(dirs[m[0]], int(m[1])) for m in [l.split(" ") for l in inp.strip().split("\n")]]
 
 
-def sim(moves):
-    trail = {"0, 0": 1}
-    h = [0, 0]
-    t = [0, 0]
-    for d, n in moves:
-        for i in range(n):
-            h = np.add(h, d)
-            # chase
-            offset = np.subtract(h, t)
-            if abs(offset[0]) > 1 or abs(offset[1]) > 1:
-                t = np.add(t, np.multiply(np.sign(offset), [1, 1]))
-                trail[str(t)] = 1
-    print(len(trail))
-
-
-def sim2(moves):
+def sim(moves, nknots):
     trail = {"0,0": 1}
     knots = [[0, 0] for _ in range(10)]
-    print(knots)
     for d, n in moves:
         for i in range(n):
             knots[0] = np.add(knots[0], d)
             # chase
-            for k in range(1, 10):
+            for k in range(1, nknots):
                 offset = np.subtract(knots[k - 1], knots[k])
                 if abs(offset[0]) > 1 or abs(offset[1]) > 1:
                     knots[k] = np.add(knots[k], np.multiply(np.sign(offset), [1, 1]))
-                    if k == 9:
-                        trail[str(knots[9][0]) + "," + str(knots[9][1])] = 1
+                    if k == nknots-1:
+                        trail[str(knots[k][0]) + "," + str(knots[k][1])] = 1
     print(len(trail))
 
 
@@ -57,8 +41,8 @@ L 5
 R 2"""
 )
 
-sim(moves)  # 13
-sim2(moves)  # 1
+sim(moves,2)  # 13
+sim(moves,10)  # 1
 
 moves = parse(
     """R 5
@@ -70,8 +54,8 @@ D 10
 L 25
 U 20"""
 )
-sim2(moves)  # 36
+sim(moves,10)  # 36
 
 moves = parse(open("9.txt").read())
-sim(moves)  # 6098
-sim2(moves)  # 2597
+sim(moves,2)  # 6098
+sim(moves,10)  # 2597
