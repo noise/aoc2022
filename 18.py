90928f86a96a8790b7eb71c6e099ddd16e340b8d
Bret Barker <bret@abitrandom.net>
2022-12-18 15:35:21 -0500

    part2 busted - WTF(lood)

--- 18.py
+++ 18.py
@@ -6,35 +6,73 @@
 import re
 import numpy as np
 
-s = 30
-
+sz = 42
 
 def parse(lines):
-    return [tuple(map(int, re.findall("\d+", l))) for l in lines]
+    return [tuple(map(int, re.findall(r"\d+", l))) for l in lines]
 
 
 def place(vox):
-    grid = np.zeros((s, s, s))
+    grid = np.zeros((sz, sz, sz), dtype=int)
     for v in vox:
-        print(v)
         grid[v] = 1
     return grid
 
 
-dirs = [[0, 0, 1], [0, 0, -1], [0, 1, 0], [0, -1, 0], [1, 0, 0], [-1, 0, 0]]
+dirs = np.array([[0, 0, 1], [0, 0, -1], [0, 1, 0], [0, -1, 0], [1, 0, 0], [-1, 0, 0]])
 
 
 def faces(grid):
     cnt = 0
-    for z in range(s - 1):
-        for y in range(s - 1):
-            for x in range(s - 1):
+    for z in range(sz - 1):
+        for y in range(sz - 1):
+            for x in range(sz - 1):
                 if grid[(x, y, z)] == 1:
                     for d in dirs:
                         c = np.add([x, y, z], d)
                         if grid[tuple(c)] == 0:
                             cnt += 1
     return cnt
+
+
+def flood(grid, x, y, z):
+    """
+    Flood fill from outside
+    """
+    q = [(x, y, z)]
+    area = 0
+
+    while len(q) > 0:
+        voxc = q.pop(0)
+        # out of bounds or visited
+        if min(voxc) < 0 or max(voxc) > sz or grid[voxc] == -1:
+            continue
+
+        grid[voxc] = -1  # visited
+
+        # count nabe faces,
+        # add empty non-visited nabes in bounds
+        for d in dirs:
+            nc = tuple(np.add(d, voxc))
+            if min(nc) >= 0 and max(nc) < sz:
+                if grid[nc] == 1:
+                    area += 1
+                elif grid[nc] == 0:
+                    q.append(nc)
+
+    return area
+
+
+def one(inpl):
+    vox = parse(inpl)
+    grid = place(vox)
+    return faces(grid)
+
+
+def two(inpl):
+    vox = parse(inpl)
+    grid = place(vox)
+    return flood(grid, 40, 40, 40)
 
 
 sinp = """2,2,2
@@ -53,15 +91,14 @@ sinp = """2,2,2
     "\n"
 )
 
-
-vox = parse(sinp)
-grid = place(vox)
-fcnt = faces(grid)
-print("1-sample:", fcnt)  # 64
-
-
 inp = open("18.txt").read().strip().split("\n")
-vox = parse(inp)
-grid = place(vox)
-fcnt = faces(grid)
-print("1. real", fcnt)  # 4302
+
+print("1-sample:", one(sinp))  # 64
+print("1-real:", one(inp))  # 4302
+
+print("2-sample:", two(sinp))  # 58
+print("2-real:", two(inp))  # 2492
+
+
+#? 2483 too low, not *2487*, not 2849, not 2851, # not 2853
+
