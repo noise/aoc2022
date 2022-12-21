59d119ea14c794ebdd431889cfc93b38ec3efe3c
Bret Barker <bret@abitrandom.net>
2022-12-21 09:31:56 -0500

    day 21 part 1

---
+++ 21.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+
+"""
+--- Day 21: Monkey Math ---
+"""
+
+
+def parse(lines):
+    mnums = {}
+    mops = []
+    for l in lines:
+        name, op = l.split(": ")
+        try:
+            op = int(op)
+            mnums[name] = op
+        except:
+            m1, op, m2 = op.split(" ")
+            mops.append((name, m1, op, m2))
+    return mnums, mops
+
+
+def sim(mnums, mops):
+    while len(mops) > 0:
+        m0, m1, op, m2 = mops.pop(0)
+        if m1 in mnums and m2 in mnums:
+            if op == "+":
+                v = mnums[m1] + mnums[m2]
+            elif op == "-":
+                v = mnums[m1] - mnums[m2]
+            elif op == "*":
+                v = mnums[m1] * mnums[m2]
+            elif op == "/":
+                v = mnums[m1] / mnums[m2]
+            else:
+                raise
+            mnums[m0] = v
+        else:
+            mops.append((m0, m1, op, m2))
+    return mnums["root"]
+
+
+def one(lines):
+    mnums, mops = parse(lines)
+    return sim(mnums, mops)
+
+
+def two(lines):
+    key = 811589153
+    nums = list(map(lambda x: x * key, parse(lines)))
+    return coords(mix(nums, 10))
+
+
+sinp = """root: pppw + sjmn
+dbpl: 5
+cczh: sllz + lgvd
+zczc: 2
+ptdq: humn - dvpt
+dvpt: 3
+lfqf: 4
+humn: 5
+ljgn: 2
+sjmn: drzm * dbpl
+sllz: 4
+pppw: cczh / lfqf
+lgvd: ljgn * ptdq
+drzm: hmdt - zczc
+hmdt: 32""".split(
+    "\n"
+)
+
+inp = open("21.txt").read().strip().split("\n")
+
+print("1-sample:", one(sinp))  #
+print("1-real:", one(inp))  #
+
+# print("2-sample:", two(sinp))  #
+# print("2-real:", two(inp))  #
